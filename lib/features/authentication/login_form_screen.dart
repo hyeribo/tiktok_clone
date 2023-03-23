80788cc72471b4ce953d638333328abd5f23f637
hyeri <harmfuli59@gmail.com>
2023-03-24 02:47:37 +0900

    관심사 화면 추가

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -19,6 +20,11 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsScreen(),
+          ),
+        );
       }
     }
   }
@@ -59,6 +65,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                       ),
                     ),
                     validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Please write your email.";
+                      }
                       return null;
                     },
                     onSaved: (newValue) {
@@ -83,6 +92,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                       ),
                     ),
                     validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Please write your password.";
+                      }
                       return null;
                     },
                     onSaved: (newValue) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -28,7 +28,7 @@ class TikTokApp extends StatelessWidget {
         ),
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: const SignUpScreen(),
+      home: const InterestsScreen(),
     );
   }
 }
