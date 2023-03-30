75d062ac6d9ebec3e69160bbd94c82975b951886
hyeri <harmfuli59@gmail.com>
2023-03-31 01:35:02 +0900

    Post Video 버튼 생성

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          // Stack 내부의 element를 움직일 수 있게 해줌
+          right: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xFF61D4F0),
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 30,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size6,
+            ),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: 18,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
