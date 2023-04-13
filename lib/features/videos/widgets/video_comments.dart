a6cbace644cb9b2f78c91d1de1148c3cfcc05583
hyeri <harmfuli59@gmail.com>
2023-04-14 00:52:15 +0900

    comment modal 열고 닫을때 비디오 pause toggle

---
+++ lib/features/videos/widgets/video_comments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoComments extends StatefulWidget {
+  const VideoComments({super.key});
+
+  @override
+  State<VideoComments> createState() => _VideoCommentsState();
+}
+
+class _VideoCommentsState extends State<VideoComments> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
