2f1355a7b566756c0b07ba74415b172840aa6c3e
hyeri <harmfuli59@gmail.com>
2023-05-16 01:17:26 +0900

    SliverGrid

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -13,8 +14,6 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          snap: true, // 스크롤을 올리는 순간 빠르게 appbar가 나타남
-          floating: true, // 스크롤을 올리면 바로 appbar가 나타남
           pinned: true, // 스크롤을 내려도 flexibleSpace가 사라지지 않음
           stretch: true, // stretchMode를 사용하기 위해 true로 설정
           backgroundColor: Colors.teal,
@@ -46,6 +45,24 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           itemExtent: 100,
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("item $index"),
+              ),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              // 허용된만큼의 무한한 grid를 생성해줌
+              maxCrossAxisExtent: 100,
+              mainAxisSpacing: Sizes.size20,
+              crossAxisSpacing: Sizes.size20,
+              childAspectRatio: 1),
         ),
       ],
     );
