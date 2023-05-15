c05c64ad084ebd88edc0dd2bf2da3bff5dee7ffb
hyeri <harmfuli59@gmail.com>
2023-05-16 00:51:02 +0900

    CustomScrollView AppBar 꾸미기

---
+++ lib/features/users/user_profile_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          // title: const Text('User Profile'),
+          backgroundColor: Colors.teal,
+          elevation: 1,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              "assets/images/placeholder.jpeg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text('User Profile'),
+          ),
+        ),
+      ],
+    );
+  }
+}
