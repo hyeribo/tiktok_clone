5f0cf1e67fb1d1d06d32e9c7330fb3cf28e3dbe2
hyeri <harmfuli59@gmail.com>
2023-05-17 03:06:26 +0900

    ListWheelScrollView 바퀴모양 리스트뷰

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -9,9 +9,24 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: Column(
-        children: const [
-          CloseButton(),
+      body: ListWheelScrollView(
+        // useMagnifier: true, // magnifier 옵션을 사용 여부
+        // magnification: 1.5, // 1.5배 확대
+        // diameterRatio: 1, // 곡률
+        itemExtent: 200,
+        children: [
+          for (var x in [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                color: Colors.teal,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Pick me",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+            ),
         ],
       ),
     );
