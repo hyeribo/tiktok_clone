bc3e5a997e8d2af651051c9246cf439fadd88fdb
hyeri <harmfuli59@gmail.com>
2023-05-25 02:53:41 +0900

    intl placeholder 추가 & 직접 import 해서 사용하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/settings/settings_screen.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:flutter_gen/gen_l10n/intl_generated.dart';
 
 void main() async {
@@ -32,18 +31,9 @@ class TikTokApp extends StatelessWidget {
       title: 'TikTok Clone',
       // flutter는 텍스트가 기본적으로 들어있는 위젯이 있다. (예: licenses)
       // 이런 위젯들은 이미 번역이 되어있으므로 include 시킨다.
-      localizationsDelegates: const [
-        AppLocalizations.delegate, // 커스텀 위젯을 위한 번역
-        GlobalMaterialLocalizations.delegate, // flutter 기본 위젯을 위한 번역
-        GlobalCupertinoLocalizations.delegate, // flutter 기본 위젯을 위한 번역
-        GlobalWidgetsLocalizations.delegate, // flutter 기본 위젯을 위한 번역
-      ],
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       // IANA language registry 에서 Subtag 국가 코드 보기
-      supportedLocales: const [
-        Locale("en"),
-        Locale("ko"),
-        Locale("es"),
-      ],
+      supportedLocales: AppLocalizations.supportedLocales,
       themeMode: ThemeMode
           .system, // light or dark: 사용자의 핸드폰 설정에 관계없이 모드 강제. system: 사용자의 핸드폰 설정에 맞춤
       theme: ThemeData(
@@ -112,7 +102,7 @@ class TikTokApp extends StatelessWidget {
         ),
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: const SettingsScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
