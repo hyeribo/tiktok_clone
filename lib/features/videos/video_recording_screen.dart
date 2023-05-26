fb7006ffde34580a5ca4be3228fb0ee66cd6aaeb
hyeri <harmfuli59@gmail.com>
2023-05-27 01:30:28 +0900

    CameraPreview

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -9,8 +12,81 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+  bool _deniedPermissions = false;
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    // 기기가 가진 카메라의 목록을 가져온다. (전면, 후면)
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {
+      _deniedPermissions = true;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Initializing camera...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size16,
+                    ),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : _cameraController.value.isInitialized
+                ? Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CameraPreview(_cameraController),
+                    ],
+                  )
+                : null,
+      ),
+    );
   }
 }
